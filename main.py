62053f9ca4c87a518862ea32360c20a1dd944444
krish03071 <>
2021-04-03 12:15:24 +0000

    Added key_alive.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import requests
 import json
 import random
 from replit import db
+from keep_alive import keep_alive
+
 
 client=discord.Client()
 
@@ -83,4 +85,6 @@ async def on_message(message):
       db["responding"]=False
     await message.channel.send("Responding is "+value)
 
+keep_alive()
 client.run(os.getenv('TOKEN')) 
+
